69f021a1c233ee08435227d317c19b545300c11a
Tural Novruzov <TuraliNovruzov@outlook.com>
2022-03-10 21:57:50 +0400

    Add project files.

---
+++ cs_client/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace cs_client
+{
+    class Post
+    {
+        [JsonPropertyName("userId")]
+        public int UserId { get; set; }
+
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        public string title { get; set; }
+        public string body { get; set; }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            HttpClient client = new HttpClient();
+
+            // var message = new HttpRequestMessage
+            // {
+            //     Method = HttpMethod.Get,
+            //     RequestUri = new Uri("https://www.google.com/")
+            //     // RequestUri = new Uri("https://jsonplaceholder.typicode.com/posts")
+            //     // RequestUri = new Uri("https://jsonplaceholder.typicode.com/posts/1")
+            // };
+            // 
+            // HttpResponseMessage response = client.SendAsync(message).Result;
+            // GetAsync(), PostAsync(), PutAsync(), DeleteAsync()
+
+
+            // Console.WriteLine(response); 
+            // Console.WriteLine(response.StatusCode); 
+
+            // var html = response.Content.ReadAsStringAsync().Result;
+            // Console.WriteLine(html);
+
+
+
+            // var json = response.Content.ReadAsStringAsync().Result;
+
+            // var post = JsonSerializer.Deserialize<Post>(json);
+            // Console.WriteLine(post.title);
+
+
+            // var posts = JsonSerializer.Deserialize<List<Post>>(json);
+            // 
+            // foreach (var post in posts)
+            // {
+            //     Console.WriteLine(post.UserId);
+            //     Console.WriteLine(post.Id);
+            //     Console.WriteLine(post.title);
+            //     Console.WriteLine(post.body);
+            // 
+            //     Console.WriteLine();
+            // }
+
+
+            // Console.WriteLine(json);
+
+
+
+            HttpResponseMessage response = client.GetAsync("http://localhost:45789/").Result;
+            Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+
+        }
+    }
+
+
+    
+
+}
+
+
+
+
+
+
+
+
+
+
+// // WebClient
+// // FTP & HTTP
+// 
+// var webClient = new WebClient();
+// Console.WriteLine(webClient.DownloadString("https://www.google.com/"));
+
+
+//  HttpWebRequest, HttpWebResponse
---
+++ cs_server/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace cs_server
+{
+    class Program
+    {
+        static void Main()
+        {
+            var listener = new HttpListener();
+            listener.Prefixes.Add("http://localhost:45789/");
+            listener.Prefixes.Add("http://localhost:45678/");
+
+            listener.Start(); // Listen, Start
+
+
+            while (true)
+            {
+                var context = listener.GetContext(); // Accept + Receive
+
+                var request = context.Request;
+                var response = context.Response;
+
+
+
+                //for (int i = 0; i < request.QueryString.Count; i++)
+                //{
+                //    Console.WriteLine(request.QueryString[i]);
+
+                //}
+
+
+                // foreach (string key in request.QueryString.Keys)
+                // {
+                //     // Console.WriteLine(request.QueryString.Get(key));
+                //     Console.WriteLine(request.QueryString[key]);
+                // }
+
+
+
+                // // Media types (mime types)
+                // response.AddHeader("Content-Type", "text/html");
+                // response.AddHeader("Content-Type", "text/plain");
+
+
+
+                StreamWriter sw = new StreamWriter(response.OutputStream);
+                sw.WriteLine("<h1 style='color:red;'> Salam Dunya <h1>");
+                // sw.Flush();
+                sw.Close();
+
+
+           
+
+
+                // response.StatusCode = 404;
+                // response.Close();
+
+            }
+        }
+    }
+}
